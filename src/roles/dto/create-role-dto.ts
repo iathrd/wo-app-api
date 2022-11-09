f5a646f8b16f9139e0e2d74601f1fbf1286bd4f1
Iqbal Athorid <iqbalathoridd@gmail.com>
2022-11-09 15:29:48 +0700

    add class validator

--- src/roles/dto/create-role-dto.ts
+++ src/roles/dto/create-role-dto.ts
@@ -1,4 +1,11 @@
+import { IsString, MinLength } from 'class-validator';
+
 export class CreateRoleDto {
+  @IsString()
+  @MinLength(4)
   name: string;
+
+  @IsString()
+  @MinLength(5)
   description: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(5)
   username: string;
+
+  @MinLength(8)
   password: string;
+
+  @IsEmail()
   email: string;
 }

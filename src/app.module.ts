d4b6f0f271832900e91ce000d3b430746dab47bd
Iqbal Athorid <iqbalathoridd@gmail.com>
2022-11-10 11:16:02 +0700

    craete modul and service auth file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { UsersModule } from './users/users.module';
     }),
     RolesModule,
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
